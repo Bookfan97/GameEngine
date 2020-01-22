7ecbbc03658c46b3bde6c7ae1dd4369ec66e9079
bookfan97 <sheltielover432@gmail.com>
2020-01-22 00:17:06 -0500

    Game Loop, pt. 1

---
+++ GameEngine/2dgameengine/src/Constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H
+#define CONSTANS_H
+
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+
+#endif
--- GameEngine/2dgameengine/src/Main.cpp
+++ GameEngine/2dgameengine/src/Main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
+
 int main(int argc, char const *argv[]) {
-  std::cout <<"Game is running..." << std::endl;
+  Game *game = new Game();
+  game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+  while(game->IsRunning())
+  {
+    game->ProcessInput();
+    game->Update();
+    game->Render();
+  }
+  game->Destroy();
   return 0;
 }
